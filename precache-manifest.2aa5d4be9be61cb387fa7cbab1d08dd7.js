262e166f987d7d9f7443c41f5cd92802d09cf013
Agnivesh Reddy <m.agnivesh.reddy@gmail.com>
2020-06-07 17:58:18 +0200

    Updates

---
+++ precache-manifest.2aa5d4be9be61cb387fa7cbab1d08dd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17bbcf2dcdd401c4c6c21b1ddc6c00b",
+    "url": "/agniveshreddy/todos/index.html"
+  },
+  {
+    "revision": "37c70fbeeae7f73e6bed",
+    "url": "/agniveshreddy/todos/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e74cf55cd76a5e15f62d",
+    "url": "/agniveshreddy/todos/static/js/2.119fa096.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/agniveshreddy/todos/static/js/2.119fa096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c70fbeeae7f73e6bed",
+    "url": "/agniveshreddy/todos/static/js/main.f3676931.chunk.js"
+  },
+  {
+    "revision": "51bd128f4dffb5fa67f2",
+    "url": "/agniveshreddy/todos/static/js/runtime-main.29169399.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/agniveshreddy/todos/static/media/logo.5d5d9eef.svg"
+  }
+]);
